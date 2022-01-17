750f595569fc5b2302986eea61a13969af167f02
AdnanYK <adnan.ykassab@gmail.com>
2022-01-17 20:53:37 +0000

    rewrote middleTest and tailTest using mocha tests

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,16 @@
 //importing functions
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const tail = require("../tail");
 
-
 //Test code
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+  it('returns two elements for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2);
+  });
+  it('ensure first element is "Lighthouse" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[0], "Lighthouse"); 
+  });
+  it('ensure second element is "Labs" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[1], "Labs");
+  });
+});
