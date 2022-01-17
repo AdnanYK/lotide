15d25dc1cc627b7e06504621cf36a8ada1b876e1
AdnanYK <adnan.ykassab@gmail.com>
2022-01-17 19:41:35 +0000

    added the test repo and modified head.js and assertEqual.js to use module.exports

---
+++ test/headTest.js
@@ -0,0 +1,9 @@
+//importing functions
+const assertEqual = require("../assertEqual");
+const head = require("../head");
+
+//Test Code
+results = head([5,6,7]);
+assertEqual(results, 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([],undefined));
